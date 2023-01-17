e880a670b01576dd7dc2426ab424ab06fe8c2599
stillawake17 <118810624+stillawake17@users.noreply.github.com>
2023-01-17 17:34:24 +0000

    broke a bit and then I tried again

--- starter/script.js
+++ starter/script.js
@@ -111,43 +111,42 @@ console.log(nCchoice);
 console.log("***************");
 
 }
-let password = UCchoice.concat(LCchoice, SCchoice, nCchoice);
-console.log(password);
+let Password = UCchoice.concat(LCchoice, SCchoice, nCchoice);
+console.log(Password);
 
 // Function to prompt user for password options
-function getPasswordOptions() {}
+function getPasswordOptions(selection) {
 // pseudocode prompt alert
 // establish length -- create array of that length or use the length as the number for the FOR loop
-
+}
 let pwdOptions = [1, 2, 3, 4];
 let selection = prompt("Choose an option 1 to 4: 1 - Upper case, 2 - upper & lower case, 3 and special or 4 and numeric?");
 switch(selection) {
   case 1:
     for (i=0; i < pwdLength; i++) {
-    let password = pwdArray.push(UCchoice); }
+    let Password = pwdArray.push(UCchoice); }
     break;
   case 2:
 // concatenate upper and lower case and then choose 16 random
 const twoOptions = UCchoice.concat(LCchoice);
 for (i=0; i < pwdLength; i++) {
-  let password = pwdArray.push(twoOptions); }
+  let Password = pwdArray.push(twoOptions); }
   break;
-
   case 3:
   const threeOptions = UCchoice.concat(LCchoice, SCchoice);
 for (i=0; i < pwdLength; i++) {
-  let password = pwdArray.push(threeOptions); }
+  let Password = pwdArray.push(threeOptions); }
   break;
 
   case 4:
     const fourOptions = UCchoice.concat(LCchoice, SCchoice, nCchoice);
   for (i=0; i < pwdLength; i++) {
-    let password = pwdArray.push(threeOptions); }
+    let Password = pwdArray.push(threeOptions); }
     break;
 
  default:
-    text = "I have never heard of that one!";
-// }
+    selection = "Pick a number between 1 and 4";
+}
 
   
 
@@ -155,24 +154,25 @@ for (i=0; i < pwdLength; i++) {
 // upper case, lower case etc.
 // this should return the options the user will choose
 // length, symbols, numbers, lowercase, uppercase
-}
+
 
 // Function for getting a random element from an array
 function getRandom(arr) {
 // should return a random element from any of the arrays
 // in Math.random() add the length of the array chosen; 
 
-for (i = 0; i < arr.length; i++) {
-Math.random( );
+
 }
-}
+
+
+
 
 // Function to generate password with user input
 function generatePassword() {
   // concatenate all the random elements from the above arrays, run loops etc
-  // your logic goes here
-
-return password;
+  // your logic goes here -- using Fisher Yates Algorithm
+ 
+return Password;
 }
 
 // Get references to the #generate element
